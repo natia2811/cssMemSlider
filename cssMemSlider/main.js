62137d9847694b4e9abf5b9b0175fc038f3d14e6
natia2811 <kor-natiya@yandex.ru>
2021-10-17 23:04:04 +0300

    feat: add js function for dots in slider

--- cssMemSlider/main.js
+++ cssMemSlider/main.js
@@ -5,10 +5,29 @@ dots = document.querySelectorAll('.btn-item');
 
 let index = 0;
 
-console.log(slides);
+
 const activeSlide = n =>{
 for(slide of slides){
     slide.classList.remove('active');
 }
-slides[n]classList.add('active');
+slides[n].classList.add('active');
 }
+
+const activeDot = n =>{
+    for(dot of dots){
+        dot.classList.remove('active');
+    }
+    dots[n].classList.add('active');
+}
+
+const prepareCurrentSlide = ind =>{
+    activeDot(ind);
+    activeSlide(ind);
+}
+
+dots.forEach((item,indexDot)=>{
+    item.addEventListener('click', () =>{
+        index = indexDot;
+        prepareCurrentSlide(index);
+    })
+})
